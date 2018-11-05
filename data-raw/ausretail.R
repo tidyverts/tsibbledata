85978b58b33a66ecd511e3705b50818740f5970c
Mitchell <mitch.ohara-wild@monash.edu>
2018-11-05 11:12:25 +1100

    Added ausretail dataset

---
+++ data-raw/ausretail.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+library(tsibble)
+
+series <- read_excel("data-raw/8501011.xlsx", sheet = 2, skip = 9) %>%
+  rename(Month = `Series ID`) %>%
+  gather(`Series ID`, Value, -Month) %>%
+  mutate(Month = yearmonth(Month))
+dict <- read_excel("data-raw/8501011.xlsx", sheet = 1, skip = 9) %>%
+  filter(`Series Type` == "Original") %>%
+  separate(`Data Item Description`, c("Category", "State", "Industry"), sep = ";", extra = "drop") %>%
+  transmute(
+    State = trimws(State),
+    Industry = trimws(Industry),
+    `Series ID`) %>%
+  filter(Industry  != "Total (Industry)")
+
+ausretail <- left_join(dict, series, by = "Series ID") %>%
+  as_tsibble(key = id(State, Industry), index = Month)
+
+usethis::use_data(ausretail, overwrite = TRUE)
