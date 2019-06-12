299b993b4adedcdd7bae875ca8e917aa44877b96
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-12 16:43:30 +1000

    Updated aus_retail to end on Dec 2018

--- data-raw/aus_retail/aus_retail.R
+++ data-raw/aus_retail/aus_retail.R
@@ -2,23 +2,28 @@ library(tidyverse)
 library(readxl)
 library(tsibble)
 
-series <- read_excel("data-raw/aus_retail/8501011.xlsx", sheet = 2, skip = 9) %>%
+series <- read_excel("data-raw/aus_retail/8501011.xls", sheet = 2, skip = 9) %>%
   rename(Month = `Series ID`) %>%
   gather(`Series ID`, Turnover, -Month) %>%
   mutate(Month = yearmonth(Month))
-dict <- read_excel("data-raw/aus_retail/8501011.xlsx", sheet = 1, skip = 9) %>%
+dict <- read_excel("data-raw/aus_retail/8501011.xls", sheet = 1, skip = 9) %>%
   filter(`Series Type` == "Original") %>%
   separate(`Data Item Description`, c("Category", "State", "Industry"), sep = ";", extra = "drop") %>%
   transmute(
     State = trimws(State),
     Industry = trimws(Industry),
-    `Series ID`) %>%
+    `Series ID`
+  ) %>%
   filter(
     Industry  != "Total (Industry)",
-    State != "Total (State)")
+    State != "Total (State)"
+  )
 
 aus_retail <- left_join(dict, series, by = "Series ID") %>%
-  filter(!is.na(Turnover)) %>%
+  filter(
+    !is.na(Turnover),
+    Month < yearmonth("2019 Jan")
+  ) %>%
   as_tsibble(key = c(State, Industry), index = Month)
 
 usethis::use_data(aus_retail, overwrite = TRUE)
