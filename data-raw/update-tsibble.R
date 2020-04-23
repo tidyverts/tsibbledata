743b03e022a26cd4c0d92b13497f890c4e3afba8
Mitchell <mail@mitchelloharawild.com>
2020-04-23 23:30:06 +1000

    Update tsibble index classes

--- data-raw/update-tsibble.R
+++ data-raw/update-tsibble.R
@@ -1,7 +1,17 @@
 library(rlang)
+library(tsibble)
 data_env <- new.env()
 lapply(list.files("data/", full.names = TRUE), load, envir = data_env)
 env_bind(data_env, !!!lapply(data_env, function(data){
+  idx <- data[[index_var(data)]]
+  if(tsibble::is_yearmonth(idx)) {
+    idx <- yearmonth(as.Date(idx))
+  } else if(tsibble::is_yearquarter(idx)) {
+    idx <- yearquarter(as.Date(idx))
+  } else if(tsibble::is_yearweek(idx)) {
+    idx <- yearweek(as.Date(idx))
+  }
+  data[[index_var(data)]] <- idx
   as_tsibble(as_tibble(data),
              regular = !is_empty(interval(data)),
              index = index_var(data),
